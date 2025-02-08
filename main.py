476546b0560016bd354256b071bbc3f10947584a
MaxKuftinec <116775534+MaxKuftinec@users.noreply.github.com>
2025-02-09 07:20:53 +0800

    Merge conflict

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, UploadFile, Form, File
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 import base64
 import jarvis
-import tempfile
+import friday
 
 # FastAPI App
 app = FastAPI()
@@ -30,12 +30,14 @@ async def compare_ui(figma_file: UploadFile = File(...), ui_file: UploadFile = F
 
     report, marked_img_base64 = jarvis.generate_comparison_report(figma_bytes, ui_bytes)
 
+    ai_generated_report = friday.report_from_openai(figma_bytes, ui_bytes, marked_img_base64, report)
+
     return {
         "status": "success",
-        "report": report,
+        "report": ai_generated_report,
         "marked_image": marked_img_base64
     }
 
 @app.get('/testapi/')
 async def testapi():
-    return JSONResponse(status_code=200, content={"message": "Api call success 42"})
+    return JSONResponse(status_code=200, content={"message": "New update with openaiAPI4"})
